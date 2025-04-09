acd332dd83f63861a87d9ccab5843cf0420b315d
Mujahid <mujahidrakibdev@gmail.com>
2025-04-09 16:15:40 +0600

    completed bot message

--- script.js
+++ script.js
@@ -123,14 +123,34 @@ function handleSubmit(e) {
     e.preventDefault()
     const userMsg = textArea.value.trim()
     if (textArea.value !== "") {
-        console.log(userMsg)
         textArea.value = ""
+        // hide the welcome message
+        chatCont.querySelector(".welcome").style.display = "none"
     }
 
+    // generate user message div and append
     const userMsgHtml = `<p class="message-text"></p>`
     const userMsgDiv = createMsgDiv(userMsgHtml, "user-message")
 
     userMsgDiv.querySelector(".message-text").textContent = userMsg
     chatCont.appendChild(userMsgDiv)
+
+
+    setTimeout(() => {
+        // generate bot message div and append after 600 ms
+        const botMsgHtml = `<div class="bot-top">
+                                <img src="images/fav-icon.svg" alt="logo" class="bot-image">
+                                <p class="message-text">Just a sec...</p> 
+                            </div>
+                            <div class="bot-bottom">
+                                <span class="material-symbols-outlined">thumb_up</span>
+                                <span class="material-symbols-outlined">thumb_down</span>
+                                <span class="material-symbols-outlined">refresh</span>
+                                <span class="material-symbols-outlined">share</span>
+                                <span class="material-symbols-outlined">more_vert</span>
+                            </div>`
+        const botMsgDiv = createMsgDiv(botMsgHtml, "bot-message")
+        chatCont.appendChild(botMsgDiv)
+    }, 600);
 }
 
